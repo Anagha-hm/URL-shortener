9a6e6c4af7b8c19c078807219c3ebc722cd5b01e
AnaghaHM <75258376+Anagha-hm@users.noreply.github.com>
2021-11-21 01:43:37 +0530

    Update trial.py

--- trial.py
+++ trial.py
@@ -1,24 +1,13 @@
 import streamlit as st
 import base62 as bs
-import pymongo
-
-client = pymongo.MongoClient(**st.secrets["mongo"])
-
-def longtoshort(m_long):
-        
-    m_short= bs.encode(m_long)
-
-    return m_short
-
-def shorttolong(m_short):
-        
-    m_long= bs.decode(m_short)
-
-    return m_long
+import psycopg2
 
 url = st.number_input('Enter/ Paste the URL', min_value=1, max_value=1000000, value=5, step=1)
-# url = st.text_input("Enter/ Paste the URL")
-# s_url=shorttolong(url)
-l_url = longtoshort(url)
+# Initialize connection.
+# Uses st.cache to only run once.
+@st.cache(allow_output_mutation=True, hash_funcs={"_thread.RLock": lambda _: None})
+def init_connection():
+    return psycopg2.connect(**st.secrets["postgres"])
 
-st.write(l_url)
+conn = init_connection()
+st.write(url)
